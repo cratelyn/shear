44709c1b6333adb2ed72a75407e6437be38e5710
katelyn martin <me+cratelyn@katelyn.world>
2024-06-26 21:57:05 -0400

    example: 💬 add an example of a simple ascii `Ellipsis`
    this uses the convenience method in `LimitedExt` as a comparison to the
    custom example added in the previous commit.

---
+++ examples/simple.rs
@@ -0,0 +1,31 @@
+//! this example program demonstrates using a custom [`Ellipsis`] when trimming strings.
+//!
+//! this program prints a collection of strings, trimming them to fit a length (in bytes).
+
+use {shear::str::LimitedExt, std::ops::Deref};
+
+pub const WIDTH: usize = 50;
+
+pub const FRUITS: &[&'static str] = &[
+    "an apple is red",
+    "a banana is yellow",
+    "a cherry is also red",
+    "a dragonfruit is black and white",
+    "a lemon is yellow",
+    "a lime is green",
+    "a watermelon is green on the outside, but red on the inside",
+];
+
+fn main() {
+    // helper fn: trim a string to `WIDTH`.
+    let trim = |s: &str| s.trim_to_length_ascii(WIDTH);
+
+    // print each element, trimming it to a fixed length in bytes.
+    FRUITS
+        .into_iter()
+        .map(Deref::deref)
+        .map(trim)
+        .for_each(|fruit| {
+            println!("{fruit}");
+        });
+}
