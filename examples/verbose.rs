69692cc127af2a42d996f4a290f9703735b96e3d
katelyn martin <me+cratelyn@katelyn.world>
2024-06-26 21:52:59 -0400

    example: 💬 add an example of a custom `Ellipsis`

---
+++ examples/verbose.rs
@@ -0,0 +1,50 @@
+//! this example program demonstrates using a custom [`Ellipsis`] when trimming strings.
+//!
+//! this program prints a collection of strings, trimming them to fit a length (in bytes).
+
+use {
+    shear::str::{Ellipsis, Limited},
+    std::ops::Deref,
+};
+
+pub const WIDTH: usize = 50;
+
+pub const FRUITS: &[&'static str] = &[
+    "an apple is red",
+    "a banana is yellow",
+    "a cherry is also red",
+    "a dragonfruit is black and white",
+    "a lemon is yellow",
+    "a lime is green",
+    "a watermelon is green on the outside, but red on the inside",
+];
+
+fn main() {
+    // helper fn: trim a string to `WIDTH`.
+    let trim = |s: &str| s.trim_to_length::<Contd>(WIDTH);
+
+    // print each element, trimming it to a fixed length in bytes.
+    FRUITS
+        .into_iter()
+        .map(Deref::deref)
+        .map(trim)
+        .for_each(|fruit| {
+            println!("{fruit}");
+        });
+}
+
+/// a verbose [`Ellipsis`].
+///
+/// provide this to [`Limited::trim_to_length()`] to print a verbose `"... (contd.)"` string at the
+/// end of a string.
+struct Contd;
+
+impl Contd {
+    const ELLIPSIS: &'static str = "... (contd.)";
+}
+
+impl Ellipsis for Contd {
+    fn ellipsis() -> &'static str {
+        Self::ELLIPSIS
+    }
+}
